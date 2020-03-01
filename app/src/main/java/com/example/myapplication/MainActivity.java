eed0369e5867a74c96d322b4434624ae17a0b647
saini1998 <sainiaaryaman1998@hmail.com>
2020-03-01 20:12:56 +0000

    Signuo page and load screen added

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,16 +1,39 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button goToLogin;
+    private Button mSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        goToLogin = (Button) findViewById(R.id.LoginID);
 
-        //Comment
+        goToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
+            }
+        });
+
+        mSignUp = (Button) findViewById(R.id.CreateAccountButtonID);
+        mSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent createAccount = new Intent (MainActivity.this, SignUpPage.class);
+                startActivity(createAccount);
+            }
+        });
+
     }
 }
+
